992e2208c2fa1d3fcfa320d772d6239aa471e333
Diego Alves <ediegoalves@gmail.com>
2025-10-13 11:04:54 -0300

    first commit

---
+++ src/controle_de_fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package controle_de_fluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
